ebe73ed7af0b59c8d34b8ee553c8e92121004e07
vicsim181 <victor.simon@outlook.com>
2021-05-05 09:50:25 +0200

    update

--- pur_beurre/settings/production.py
+++ pur_beurre/settings/production.py
@@ -8,9 +8,9 @@ ALLOWED_HOSTS = ['167.99.142.3']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql', # we use the postgresql adaptator
-        'NAME': 'purbeurre',
+        'NAME': os.getenv('POSTGRESQL_DB_NAME'),
         'USER': os.getenv('POSTGRESQL_USER'),
-        'PASSWORD': os.getenv('POSTRESQL_PSWD'),
+        'PASSWORD': os.getenv('POSTGRESQL_PSWD'),
         'HOST': '',
         'PORT': '5432',
     }
