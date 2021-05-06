213f2fd932469b5efd382ec69c1a5041337b4ae4
vicsim181 <victor.simon@outlook.com>
2021-05-06 11:26:40 +0200

    Update base.py

--- pur_beurre/settings/base.py
+++ pur_beurre/settings/base.py
@@ -11,8 +11,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
