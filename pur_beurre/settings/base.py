dd3220c9f444e164e21bde39d3c3303546351059
vicsim181 <victor.simon@outlook.com>
2021-05-21 11:21:24 +0200

    update path in db_feed

--- pur_beurre/settings/base.py
+++ pur_beurre/settings/base.py
@@ -14,7 +14,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
